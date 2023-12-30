95655564bc650209cb3ef56a1f17552e00b3f5da
Parvez Alam <parvezimohammed@gmail.com>
2023-12-31 01:49:27 +0530

    Bk-Infra

---
+++ src/test/java/app/booking/Resortle/ResortleApplicationTests.java
@@ -0,0 +1,13 @@
+package app.booking.Resortle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResortleApplicationTests {
+
+	/*@Test
+	void contextLoads() {
+	}*/
+
+}
