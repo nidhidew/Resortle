be922a08d1544d3e54ae3bb1b369eda502d328ce
Nidhi Dewangan <41151235+nidhidew@users.noreply.github.com>
2024-01-10 23:50:20 +0530

    login form is added for admin,manager,customer

--- ResortBooking/resort_booking/src/Components/Lending/Login.jsx
+++ ResortBooking/resort_booking/src/Components/Lending/Login.jsx
@@ -1,22 +1,105 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+  const [selectedAdminBlock, setAdminBlock] = useState(false);
+  const [selectedManagerBlock, setManagerBlock] = useState(false);
+  const [selectedCustomerBlock, setCustomerBlock] = useState(false);
+  const [hideBlocks, setHideBlocks] = useState(false);
+
+  const handleClickAdmin = () => {
+    setAdminBlock(true);
+    setHideBlocks(true); 
+  };
+
+  const handleClickCustomer = () => {
+    setCustomerBlock(true);
+    setHideBlocks(true); 
+  };
+
+  const handleClickManager = () => {
+    setManagerBlock(true);
+    setHideBlocks(true); 
+  };
+
   return (
     <div className="bg-login">
-      <div className="admin-block">
-        <div className="w-64 h-64 bg-gray-200 border border-gray-400 rounded-md p-4">
-          <h1 className="block-text">Admin</h1>
-        </div>
-      </div>
-      <div className="manager-block">
-        <div className="w-64 h-64 bg-gray-200 border border-gray-400 rounded-md p-4">
-          <h1 className="block-text">Manager</h1>
-        </div>
-      </div>
-      <div className="customer-block">
-        <div className="w-64 h-64 bg-gray-200 border border-gray-400 rounded-md p-4">
-          <h1 className="block-text">Customer</h1>
-        </div>
+      <div>
+        {!hideBlocks && (
+          <>
+            <div className="admin-block" onClick={handleClickAdmin}>
+              <div className="w-64 h-64 bg-gray-200 border border-gray-400 rounded-md p-4">
+                <h1 className="block-text">Admin</h1>
+              </div>
+            </div>
+            <div className="manager-block" onClick={handleClickManager}>
+              <div className="w-64 h-64 bg-gray-200 border border-gray-400 rounded-md p-4">
+                <h1 className="block-text">Manager</h1>
+              </div>
+            </div>
+            <div className="customer-block" onClick={handleClickCustomer}>
+              <div className="w-64 h-64 bg-gray-200 border border-gray-400 rounded-md p-4">
+                <h1 className="block-text">Customer</h1>
+              </div>
+            </div>
+          </>
+        )}
+
+        {selectedAdminBlock && (
+          <div className="admin-login" id="admin-login">
+            <div className="w-64 h-64 bg-gray-200 border border-gray-400 rounded-md p-4 admin-login-block">
+              <h1 className="block-text-admin">Admin Login</h1>
+
+              <div>
+                <form className="center-form">
+                  <div><label className="block text-sm font-medium leading-6 text-gray-900 label-form">Username:  </label>
+                  <input type="text" name="username" className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 input-form"/></div><br></br>
+                  <div><label>Password:  </label>
+                  <input type="password" name="password" /></div><br></br>
+                  <button type="submit">Login</button><br></br>
+                  <h3>New User ? <a>Register</a></h3>
+                </form>
+              </div>
+            </div>
+          </div>
+        )}
+
+        {selectedManagerBlock && (
+          <div className="manager-login" id="manager-login">
+            <div className="w-64 h-64 bg-gray-200 border border-gray-400 rounded-md p-4 admin-login-block">
+              <h1 className="block-text-admin">Hotel Manager Login</h1>
+
+              <div>
+                <form>
+                  <div><label>Username:  </label>
+                  <input type="text" name="username" /></div><br></br>
+                  <div><label>Password:  </label>
+                  <input type="password" name="password" /></div><br></br>
+                  <button type="submit">Login</button>
+                  <h3>New User ? <a>Register</a></h3>
+                </form>
+              </div>
+            </div>
+          </div>
+        )}
+
+        {selectedCustomerBlock && (
+          <div className="customer-login" id="customer-login">
+            <div className="w-64 h-64 bg-gray-200 border border-gray-400 rounded-md p-4 admin-login-block">
+              <h1 className="block-text-admin">Customer Login</h1>
+
+              <div>
+                <form>
+                  <div><label>Username:  </label>
+                  <input type="text" name="username" /></div><br></br>
+                  <div><label>Password:  </label>
+                  <input type="password" name="password" /></div><br></br>
+                  <button type="submit">Login</button>
+                  <h3>New User ? <a>Register</a></h3>
+                </form>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
