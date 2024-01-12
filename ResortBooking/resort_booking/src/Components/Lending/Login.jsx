3ab112965efff42aa0acb76baf2df3d67bb79dc7
Nidhi Dewangan <41151235+nidhidew@users.noreply.github.com>
2024-01-12 22:39:12 +0530

    register form is added for customer (#9)

--- ResortBooking/resort_booking/src/Components/Lending/Login.jsx
+++ ResortBooking/resort_booking/src/Components/Lending/Login.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from 'react-router-dom';
 
 const Login = () => {
   const [selectedAdminBlock, setAdminBlock] = useState(false);
@@ -65,8 +66,8 @@ const Login = () => {
                   <div><label className="block text-sm font-medium leading-6 text-gray-900 label-form password-label-login">Password:  </label>
                   <input type="password" name="password" className="password-input-login block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 input-form"/></div><br></br>
                   <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded login-submit">Login</button><br></br><br></br>
-                  <h3>New User ? <a>Register</a></h3><br></br>
-                  <h3>Wrong Login ?{" "}<a href="#" onClick={handleGoBack}>Go Back</a></h3>
+                  {/* <h3>New User ? <Link to="/registeration">Register</Link></h3><br></br>
+                  <h3>Wrong Login ?{" "}<a href="#" onClick={handleGoBack}>Go Back</a></h3> */}
                 </form>
               </div>
             </div>
@@ -85,8 +86,8 @@ const Login = () => {
                   <div><label className="block text-sm font-medium leading-6 text-gray-900 label-form password-label-login">Password:  </label>
                   <input type="password" name="password" className="password-input-login block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 input-form"/></div><br></br>
                   <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded login-submit">Login</button><br></br><br></br>
-                  <h3>New User ? <a>Register</a></h3><br></br>
-                  <h3>Wrong Login ?{" "}<a href="#" onClick={handleGoBack}>Go Back</a></h3>
+                  {/* <h3>New User ? <Link to="/registeration">Register</Link></h3><br></br>
+                  <h3>Wrong Login ?{" "}<a href="#" onClick={handleGoBack}>Go Back</a></h3> */}
                 </form>
               </div>
             </div>
@@ -105,7 +106,7 @@ const Login = () => {
                   <div><label className="block text-sm font-medium leading-6 text-gray-900 label-form password-label-login">Password:  </label>
                   <input type="password" name="password" className="password-input-login block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 input-form"/></div><br></br>
                   <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded login-submit">Login</button><br></br><br></br>
-                  <h3>New User ? <a>Register</a></h3><br></br>
+                  <h3>New User ? <Link to="/registeration">Register</Link></h3><br></br>
                   <h3>Wrong Login ?{" "}<a href="#" onClick={handleGoBack}>Go Back</a></h3>
                 </form>
               </div>
