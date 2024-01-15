3411461abf2c33f8408da327d286159903095e72
Nidhi Dewangan <41151235+nidhidew@users.noreply.github.com>
2024-01-15 23:07:26 +0530

    added background image

--- ResortBooking/resort_booking/src/Components/Lending/Single_Room.jsx
+++ ResortBooking/resort_booking/src/Components/Lending/Single_Room.jsx
@@ -3,8 +3,23 @@ import React from 'react'
 const Single_Room = () => {
   return (
     <div>
-      <div className='bg-feature'>
-        <h1>Single Room</h1>
+      <div className='bg_section_1_single'>
+        {/* <h1>Single Room</h1> */}
+        <div className="container_1">
+            <center>
+              <h1 className="header_container_1">SINGLE BED ROOMS</h1>
+            </center>
+            <p className="underline_container_1"></p><br></br>
+        </div>
+      </div>
+      <div className=''>
+        <div>
+          <h1>
+            Details
+          </h1>
+          <p>Experience comfort and tranquility in our Deluxe Single Room, thoughtfully designed to provide a peaceful sanctuary for solo travelers. Whether you're on a business trip or 
+            simply seeking a cozy space to unwind, our single-bed room offers the perfect blend of style and functionality.</p>
+        </div>
       </div>
     </div>
   )
