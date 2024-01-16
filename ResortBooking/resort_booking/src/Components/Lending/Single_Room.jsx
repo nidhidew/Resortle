274a3eace441509b9975ca66e55985304e5edc34
Nidhi Dewangan <41151235+nidhidew@users.noreply.github.com>
2024-01-16 22:50:02 +0530

    single page feature completed

--- ResortBooking/resort_booking/src/Components/Lending/Single_Room.jsx
+++ ResortBooking/resort_booking/src/Components/Lending/Single_Room.jsx
@@ -13,13 +13,70 @@ const Single_Room = () => {
         </div>
       </div>
       <div className=''>
-        <div>
-          <h1>
+        <div className='single-info'>
+          <h1 className='single-heading'>Info</h1>
+          <h3 className='p-single'>Price : 800/-</h3>
+          <h3 className='p-single'>Size : 500 SQFT</h3>
+          <h3 className='p-single'>Max Capacity : 2 People</h3>
+          <h3 className='p-single'>Pets Allowed</h3>
+          <h3 className='p-single'>Free Breakfast Included</h3>
+        </div>
+        <div className='single-details'>
+          <h1 className='single-heading'>
             Details
           </h1>
-          <p>Experience comfort and tranquility in our Deluxe Single Room, thoughtfully designed to provide a peaceful sanctuary for solo travelers. Whether you're on a business trip or 
+          <p className='p-single'>Experience comfort and tranquility in our Deluxe Single Room, thoughtfully designed to provide a peaceful sanctuary for solo travelers. Whether you're on a business trip or 
             simply seeking a cozy space to unwind, our single-bed room offers the perfect blend of style and functionality.</p>
         </div>
+        <div className='single-key'>
+          <h1 className='single-heading'>
+            Key Features
+          </h1>
+          <ul>
+            <li>
+              <h3 className='h3-single'><b>Single Bed : </b></h3>
+              <p className='p-single'>Drift into a restful sleep on a plush, premium-quality single bed. Fresh linens and a selection of pillows ensure a comfortable night's sleep.</p>
+            </li>
+            <li>
+              <h3 className='h3-single'><b>Modern Decor : </b></h3>
+              <p className='p-single'>Immerse yourself in a contemporary ambiance with tastefully curated decor. Elegant furnishings and soothing color palettes create a welcoming atmosphere.</p>
+            </li>
+            <li>
+              <h3 className='h3-single'><b>Workstation : </b></h3>
+              <p className='p-single'>Stay productive with a dedicated workspace equipped with high-speed Wi-Fi. Perfect for business travelers, the room is designed to meet your professional needs.</p>
+            </li>
+            <li>
+              <h3 className='h3-single'><b>Private Bathroom : </b></h3>
+              <p className='p-single'>Enjoy the convenience of a private en-suite bathroom featuring modern fixtures, a spacious shower, and complimentary toiletries.</p>
+            </li>
+            <li>
+              <h3 className='h3-single'><b>Entertainment : </b></h3>
+              <p className='p-single'>Unwind with a flat-screen TV offering a variety of channels. Whether catching up on the latest news or enjoying a movie night, entertainment is at your fingertips.</p>
+            </li>
+            <li>
+              <h3 className='h3-single'><b>Complimentary Amenities : </b></h3>
+              <p className='p-single'>Indulge in complimentary amenities, including a minibar, coffee/tea maker, and daily housekeeping services.</p>
+            </li>
+          </ul>
+        </div>
+        <div className='single-services'>
+          <h1 className='single-heading'>Additional Services</h1>
+          <ul>
+            <li>
+              <h3 className='h3-single'><b>Room Services : </b></h3>
+              <p className='p-single'>Delight in the convenience of in-room dining with our 24/7 room service, offering a diverse menu of culinary delights.</p>
+            </li>
+            <li>
+              <h3 className='h3-single'><b>Concierge Assistance : </b></h3>
+              <p className='p-single'>Our dedicated concierge team is ready to assist you with any travel arrangements, local recommendations, or special requests.</p>
+            </li>
+            <li>
+              <h3 className='h3-single'><b>Fitness Center Access : </b></h3>
+              <p className='p-single'>Stay active during your stay with complimentary access to our well-equipped fitness center.</p>
+            </li>
+          </ul>
+        </div>
+        
       </div>
     </div>
   )
