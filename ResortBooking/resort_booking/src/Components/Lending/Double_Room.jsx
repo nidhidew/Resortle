c85cb4ebf8399161394d8ddf902537f633aa68b7
Nidhi Dewangan <41151235+nidhidew@users.noreply.github.com>
2024-01-17 23:43:55 +0530

    double room page updated (#14)

--- ResortBooking/resort_booking/src/Components/Lending/Double_Room.jsx
+++ ResortBooking/resort_booking/src/Components/Lending/Double_Room.jsx
@@ -3,9 +3,58 @@ import React from 'react'
 const Double_Room = () => {
   return (
     <div>
-      <div className='bg-feature'>
-        <h1>Double Room</h1>
+      <div className='bg_section_1_double'>
+        <div className="container_1">
+            <center>
+              <h1 className="header_container_1">DOUBLE BED ROOMS</h1>
+            </center>
+            <p className="underline_container_1"></p><br></br>
         </div>
+      </div>
+      <div>
+        <div>
+
+        </div>
+        <div className='double-details'>
+          <h1 className=''>Details</h1>
+          <p>Discover comfort and tranquility in our elegantly designed Double Bed Room, the perfect choice for couples or travelers seeking extra space and a restful retreat.</p>
+        </div>
+        <div className='double-key'>
+          <h1>
+            Key Features:
+          </h1>
+          <ul>
+            <li>
+              <h3><b>Bedding :</b></h3>
+              <p>Enjoy a luxurious and spacious double bed, adorned with premium linens to ensure a restful night's sleep.</p>
+            </li>
+            <li>
+              <h3><b>Amenities :</b></h3>
+              <p>Unwind in a room equipped with modern amenities, including a flat-screen TV, complimentary Wi-Fi, and climate control for your convenience.</p>
+            </li>
+            <li>
+              <h3><b>Workspace :</b></h3>
+              <p>For those traveling for business, a dedicated workspace is provided to meet your professional needs, ensuring you stay connected and productive.</p>
+            </li>
+            <li>
+              <h3><b>Bathroom :</b></h3>
+              <p>Refresh in our well-appointed bathroom featuring a shower, plush towels, and complimentary toiletries.</p>
+            </li>
+          </ul>
+        </div>
+        <div className='double-services'>
+          <h1>Additional Services</h1>
+          <ul>
+            <li>
+              <h3><b>Room Services : </b></h3>
+            <p>Room service available for your convenience.</p>
+            </li>
+            <li>
+            Complimentary breakfast to start your day right.
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
