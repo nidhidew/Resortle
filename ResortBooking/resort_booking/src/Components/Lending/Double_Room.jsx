7ae619052b81b3bc50b161eee5782c9bc99b444f
Nidhi Dewangan <41151235+nidhidew@users.noreply.github.com>
2024-01-18 22:37:22 +0530

    double room page is completed

--- ResortBooking/resort_booking/src/Components/Lending/Double_Room.jsx
+++ ResortBooking/resort_booking/src/Components/Lending/Double_Room.jsx
@@ -1,62 +1,112 @@
-import React from 'react'
+import React from "react";
 
 const Double_Room = () => {
   return (
     <div>
-      <div className='bg_section_1_double'>
+      <div className="bg_section_1_double">
         <div className="container_1">
-            <center>
-              <h1 className="header_container_1">DOUBLE BED ROOMS</h1>
-            </center>
-            <p className="underline_container_1"></p><br></br>
+          <center>
+            <h1 className="header_container_1">DOUBLE BED ROOMS</h1>
+          </center>
+          <p className="underline_container_1"></p>
+          <br></br>
         </div>
       </div>
-      <div>
-        <div>
-
+      <div className="">
+        <div className="single-info">
+          <h1 className="single-heading">Info</h1>
+          <h3 className="p-single">Price : 800/-</h3>
+          <h3 className="p-single">Size : 500 SQFT</h3>
+          <h3 className="p-single">Max Capacity : 2 People</h3>
+          <h3 className="p-single">Pets Allowed</h3>
+          <h3 className="p-single">Free Breakfast Included</h3>
         </div>
-        <div className='double-details'>
-          <h1 className=''>Details</h1>
-          <p>Discover comfort and tranquility in our elegantly designed Double Bed Room, the perfect choice for couples or travelers seeking extra space and a restful retreat.</p>
+        <div className="single-details">
+          <h1 className="single-heading">Details</h1>
+          <p className="p-single">
+            Discover comfort and tranquility in our elegantly designed Double
+            Bed Room, the perfect choice for couples or travelers seeking extra
+            space and a restful retreat.
+          </p>
         </div>
-        <div className='double-key'>
-          <h1>
-            Key Features:
-          </h1>
+        <div className="single-key">
+          <h1 className="single-heading">Key Features:</h1>
           <ul>
             <li>
-              <h3><b>Bedding :</b></h3>
-              <p>Enjoy a luxurious and spacious double bed, adorned with premium linens to ensure a restful night's sleep.</p>
+              <h3 className="h3-single">
+                <b>Bedding :</b>
+              </h3>
+              <p className="p-single">
+                Enjoy a luxurious and spacious double bed, adorned with premium
+                linens to ensure a restful night's sleep.
+              </p>
             </li>
             <li>
-              <h3><b>Amenities :</b></h3>
-              <p>Unwind in a room equipped with modern amenities, including a flat-screen TV, complimentary Wi-Fi, and climate control for your convenience.</p>
+              <h3 className="h3-single">
+                <b>Amenities :</b>
+              </h3>
+              <p className="p-single">
+                Unwind in a room equipped with modern amenities, including a
+                flat-screen TV, complimentary Wi-Fi, and climate control for
+                your convenience.
+              </p>
             </li>
             <li>
-              <h3><b>Workspace :</b></h3>
-              <p>For those traveling for business, a dedicated workspace is provided to meet your professional needs, ensuring you stay connected and productive.</p>
+              <h3 className="h3-single">
+                <b>Workspace :</b>
+              </h3>
+              <p className="p-single">
+                For those traveling for business, a dedicated workspace is
+                provided to meet your professional needs, ensuring you stay
+                connected and productive.
+              </p>
             </li>
             <li>
-              <h3><b>Bathroom :</b></h3>
-              <p>Refresh in our well-appointed bathroom featuring a shower, plush towels, and complimentary toiletries.</p>
+              <h3 className="h3-single">
+                <b>Bathroom :</b>
+              </h3>
+              <p className="p-single">
+                Refresh in our well-appointed bathroom featuring a shower, plush
+                towels, and complimentary toiletries.
+              </p>
             </li>
           </ul>
         </div>
-        <div className='double-services'>
-          <h1>Additional Services</h1>
+        <div className="single-services">
+          <h1 className="single-heading">Additional Services</h1>
           <ul>
             <li>
-              <h3><b>Room Services : </b></h3>
-            <p>Room service available for your convenience.</p>
+              <h3 className="h3-single">
+                <b>Room Services : </b>
+              </h3>
+              <p className="p-single">
+                Delight in the convenience of in-room dining with our 24/7 room
+                service, offering a diverse menu of culinary delights.
+              </p>
             </li>
             <li>
-            Complimentary breakfast to start your day right.
+              <h3 className="h3-single">
+                <b>Concierge Assistance : </b>
+              </h3>
+              <p className="p-single">
+                Our dedicated concierge team is ready to assist you with any
+                travel arrangements, local recommendations, or special requests.
+              </p>
+            </li>
+            <li>
+              <h3 className="h3-single">
+                <b>Fitness Center Access : </b>
+              </h3>
+              <p className="p-single">
+                Stay active during your stay with complimentary access to our
+                well-equipped fitness center.
+              </p>
             </li>
           </ul>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Double_Room
+export default Double_Room;
